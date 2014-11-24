b77e9e5df486bb7994e0fd5b04fc092201020a4e
Giampaolo Bellavite <gpberlin@gmail.com>
2014-11-24 23:53:52 +0100

    Fix for undefined module

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,9 @@
 'use strict';
 
-var React       = require('react');
-var Router      = require('react-router');
-var reactRoutes = require('./react-routes.jsx');
-var title       = require('./helpers/document-title');
+var React       	= require('react');
+var Router      	= require('react-router');
+var reactRoutes 	= require('./react-routes.jsx');
+var documentTitle = require('./helpers/document-title');
 
 // Render the current route server-side using views/page.ejs 
 var routes = function (req, res, next) {
@@ -11,7 +11,7 @@ var routes = function (req, res, next) {
 		
 		var handlerElement = React.createElement(Handler);
 		var html           = React.renderToString(handlerElement);
-		var title          = title(state.routes, state.params);
+		var title          = documentTitle(state.routes, state.params);
 
 		res.render('page', {
 			title: title,
