a12d74accd1581b18308cd38b697040bb2c88754
Giampaolo Bellavite <gpberlin@gmail.com>
2014-11-25 11:19:32 +0100

    Move dev server in another script

---
+++ server-dev.js
@@ -0,0 +1,49 @@
+'use strict';
+
+var debug            = require('debug')('app');
+var webpack          = require('webpack');
+var WebpackDevServer = require('webpack-dev-server');
+var webpackConfig    = require('./webpack.dev');
+var livereload       = require('livereload');
+var fs               = require('fs');
+var sass             = require('node-sass');
+
+// Start the main server
+var server           = require('./server');
+
+// Run the webpack dev server
+var webpackServer = new WebpackDevServer(webpack(webpackConfig), {
+	publicPath: webpackConfig.output.publicPath,
+	contentBase: 'http://localhost:3000',
+	noInfo: true,
+	hot: true,
+	headers: { "Access-Control-Allow-Origin": "*" }
+}).listen(3001, 'localhost', function (err, result) {
+	if (err) console.log(err);
+	else debug('Webpack server listening on port 3001');
+});
+
+// Render scss files
+var renderSass = function(filename) {
+	sass.renderFile({
+		file: __dirname + '/app/style/main.scss',
+		outFile: __dirname + '/app/public/css/main.css',
+		sourceMap: true,
+		success: function (css) { if (filename) debug('Changed '+ filename); },
+		error: function (error) { console.log(error); }
+	});
+};
+
+// Watch for scss changes
+fs.watch(__dirname + '/app/style', function (event, filename) {
+	var ext = filename.split('/').pop().split('.').pop();
+	if (ext !== 'scss') return;
+	renderSass(filename);
+});
+
+// Render for the first run
+renderSass();
+
+// Watch public dir with livereload
+var lr = livereload.createServer();
+lr.watch(__dirname + '/app/public');
--- server.js
+++ server.js
@@ -1,32 +1,14 @@
-"use strict";
+'use strict';
 
 // enables JSX requires
-require('node-jsx').install({extension: '.jsx'});
+require('node-jsx').install({ extension: '.jsx' });
 
 var debug = require('debug')('app');
 var app = require('./app');
-
 app.set('port', process.env.PORT || 3000);
 
 var server = app.listen(app.get('port'), function () {
-	debug('Express server listening on port ' + server.address().port);
+	debug('Express ' + app.get('env') + ' server listening on port ' + server.address().port);
 });
 
-if (app.get('env') === 'development') {
-	var webpack = require('webpack');
-	var WebpackDevServer = require('webpack-dev-server');
-	var config = require('./webpack.config.dev');
-
-	var webpackServer = new WebpackDevServer(webpack(config), {
-		publicPath: config.output.publicPath,
-		contentBase: 'http://localhost:3000',
-		noInfo: true,
-		hot: true
-	}).listen(3001, 'localhost', function (err, result) {
-		if (err) {
-			console.log(err);
-		} else {
-			debug('Webpack server listening on port 3001');
-		}
-	});
-}
+module.exports = server;
