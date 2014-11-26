29db502fb02ee09ac28cd54c9b1538eae8e6feaf
Giampaolo Bellavite <gpberlin@gmail.com>
2014-11-26 15:47:19 +0100

    Remove useless props attribute

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,25 +1,21 @@
 'use strict';
 
-var React       	= require('react');
-var Router      	= require('react-router');
-var reactRoutes 	= require('./react-routes.jsx');
+var React         = require('react');
+var Router        = require('react-router');
+var reactRoutes   = require('./react-routes.jsx');
 var documentTitle = require('./helpers/document-title');
 
 // Render the current route server-side using views/page.ejs 
 var routes = function (req, res, next) {
-	Router.run(reactRoutes, req.path, function (Handler, state) {
-		
-		var handlerElement = React.createElement(Handler);
-		var html           = React.renderToString(handlerElement);
-		var title          = documentTitle(state.routes, state.params);
+  Router.run(reactRoutes, req.path, function (Handler, state) {
+    
+    var handlerElement = React.createElement(Handler);
+    var html           = React.renderToString(handlerElement);
+    var title          = documentTitle(state.routes, state.params);
 
-		res.render('page', {
-			title: title,
-			html: html,
-			props: handlerElement.props
-		});
+    res.render('page', { title: title, html: html });
 
-	});
+  });
 };
 
 module.exports = routes;
