b16f4c6bbeb3eb90df404852a361537d91cd05e9
Giampaolo Bellavite <gpberlin@gmail.com>
2014-12-02 00:14:36 +0100

    Indentation

--- app/pages/HomePage.jsx
+++ app/pages/HomePage.jsx
@@ -6,11 +6,11 @@ var Home = React.createClass({
 
   render: function () {
     return (
-    	<p>
-    		Set a repository or try
-     		with <Router.Link to="repo" params={{ login: 'facebook', name: 'react'}}>facebook/react
-     		</Router.Link> or <Router.Link to="user" params={{ login: 'gpbl' }}>gpbl</Router.Link>
-     	</p>
+      <p>
+        Set a repository or try
+        with <Router.Link to="repo" params={{ login: 'facebook', name: 'react'}}>facebook/react
+        </Router.Link> or <Router.Link to="user" params={{ login: 'gpbl' }}>gpbl</Router.Link>
+      </p>
      );
   }
 });
