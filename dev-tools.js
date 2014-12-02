6bf02968a2a04ff71216f6dc859417afe180f23c
Giampaolo Bellavite <gpberlin@gmail.com>
2014-12-02 13:36:17 +0100

    Use material-ui

--- dev-tools.js
+++ dev-tools.js
@@ -1,12 +1,12 @@
 'use strict';
 
-var debug            = require('debug')('app');
-var webpack          = require('webpack');
+var debug = require('debug')('app');
+var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
-var webpackConfig    = require('./webpack.config.dev');
-var livereload       = require('livereload');
-var fs               = require('fs');
-var sass             = require('node-sass');
+var webpackConfig = require('./webpack.config.dev');
+var livereload = require('livereload');
+var fs = require('fs');
+var less = require('less');
 
 // Run the webpack dev server
 var webpackServer = new WebpackDevServer(webpack(webpackConfig), {
@@ -19,29 +19,39 @@ var webpackServer = new WebpackDevServer(webpack(webpackConfig), {
   }
 }).listen(3001, 'localhost', function (err, result) {
   if (err) console.log(err);
-  else  debug('Webpack server listening on port 3001');
+  else debug('Webpack server listening on port 3001');
 });
 
-// Render scss files
-var renderSass = function(filename) {
-  sass.renderFile({
-    file: __dirname + '/app/style/main.scss',
-    outFile: __dirname + '/app/public/css/main.css',
-    sourceMap: true,
-    success: function (css) { if (filename) debug('Changed '+ filename); },
-    error: function (error) { console.log(error); }
+
+var renderLess = function (changedFileName) {
+  fs.readFile('./app/style/main.less', function (err, data) {
+    var dataString = data.toString();
+    less.render(dataString, {
+      paths: [__dirname + '/app/style'],
+      filename: 'main.less',
+      sourceMap: {
+        sourceMapFileInline: true
+      }
+    }, function (err, output) {
+      if (err) {
+        console.log(err.type + ' Error: ' + err.message + ' in ' + err.filename + ':' + err.index + ':' + err.line);
+        return;
+      }
+      fs.writeFileSync('./app/public/css/main.css', output.css, 'utf8');
+      if (changedFileName) debug('Changed ' + changedFileName + ', main.css rendered.');
+    });
   });
 };
 
-// Watch for scss changes
+// Watch for css changes
 fs.watch(__dirname + '/app/style', function (event, filename) {
   var ext = filename.split('/').pop().split('.').pop();
-  if (ext !== 'scss') return;
-  renderSass(filename);
+  if (ext !== 'less') return;
+  renderLess(filename);
 });
 
 // Render for the first run
-renderSass();
+renderLess();
 
 // Watch public dir with livereload
 var lr = livereload.createServer();
