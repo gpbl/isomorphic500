126d73a0e9d7b1bad270c2d48dc008ef062f5cf3
Giampaolo Bellavite <gpberlin@gmail.com>
2014-12-03 12:00:27 +0100

    Remove debugger

--- app/api/api.js
+++ app/api/api.js
@@ -3,7 +3,6 @@
 var superagent = require('superagent');
 var API_ROOT = 'https://api.github.com';
 
-// debugger;
 module.exports = {
 	request: function (path) {
 		return superagent(API_ROOT + path);
