d7434eb67b3633c0e127dd21757b595df9d590e6
gpbl <io@gpbl.org>
2015-01-12 01:11:07 +0100

    Display photos from state

--- components/Home.jsx
+++ components/Home.jsx
@@ -1,8 +1,31 @@
 import React from 'react';
+import { StoreMixin } from 'fluxible-app';
+import PhotosStore from '../stores/PhotosStore';
 
 const Home = React.createClass({
+  mixins: [StoreMixin],
+  statics: {
+    storeListeners: [PhotosStore]
+  },
+  getInitialState() {
+    console.log('getInitialState of Home.jsx', this.getStore(PhotosStore).getState().photos.length, 'photos');
+    return this.getStore(PhotosStore).getState();
+  },
+  onChange() {
+    const state = this.getStore(CustomersStore).getState();
+    this.setState(state);
+  },
   render() {
-    return <p>Welcome to the home page!</p>;
+    return (
+      <div>
+        <p>Welcome to the home page!</p>;
+        { 
+          this.state.photos.map((photo) => {
+            return <p>{ photo.title }</p>;
+          })
+        }
+      </div>
+    );
   }
 });
 
