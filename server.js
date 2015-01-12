bba823158e2cdebcdba0f4cc2b26a61ea9e24b83
gpbl <io@gpbl.org>
2015-01-12 01:08:02 +0100

    Fix for ssl

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 // enable ES6
 require("6to5/register");
 
+// fix ssl issues https://github.com/visionmedia/superagent/issues/188
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
 // start express server
 require('./server/index');
