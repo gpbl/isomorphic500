144280aa8e6e480daba84c7be0d641143db7b14a
gpbl <io@gpbl.org>
2015-01-13 18:45:24 +0100

    Cleanup

--- services/i18n.js
+++ services/i18n.js
@@ -1,19 +1,12 @@
-
 // store for getting i18n data
-
 export default {
-
   name: 'i18n',
-
   read(req, resource, params, config, callback) {
     try {
       const i18n = require(`../i18n/${params.locale}.js`);
-
       callback(null, i18n);
-      
       if (process.env.NODE_ENV === 'development')
         delete require.cache[require.resolve(`../i18n/${params.locale}.js`)];
-
     }
     catch(e) { callback(e); }
   }
