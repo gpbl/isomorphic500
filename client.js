2b25e919d64a4f063ba34b0811f5983106621f8a
gpbl <io@gpbl.org>
2015-01-13 01:01:51 +0100

    Cleanup

--- client.js
+++ client.js
@@ -4,13 +4,14 @@ import app from './app';
 const dehydratedState = window.App;
 
 app.rehydrate(dehydratedState, function (err, context) {
-    if (err) throw err;
-    window.context = context;
-    React.render(
-      app.getAppComponent()( { 
-        context: context.getComponentContext() 
-      }), 
-      document.getElementById('mountNode'), 
-      () => { console.log('Application rendered on mountNode.'); }
-    );
+  if (err) throw err;
+  window.context = context;
+  const AppComponent = app.getAppComponent();
+  React.render(
+    AppComponent({ 
+      context: context.getComponentContext() 
+    }), 
+    document.getElementById('mountNode'), 
+    () => { console.log('Application rendered on mountNode.'); }
+  );
 });
