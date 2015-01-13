66630b80e4b02676e0c19012e606dbb5d9581028
gpbl <io@gpbl.org>
2015-01-13 01:00:52 +0100

    Add node's intl shim

--- server.js
+++ server.js
@@ -4,5 +4,7 @@ require("6to5/register");
 // fix ssl issues https://github.com/visionmedia/superagent/issues/188
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
+if (!global.Intl) require('intl');
+
 // start express server
 require('./server/index');
