96711b5c6eea96002ab7b9008d89b31221a3c242
gpbl <io@gpbl.org>
2015-01-13 17:38:24 +0100

    Do not cache i18n files on dev

--- services/i18n.js
+++ services/i18n.js
@@ -8,7 +8,12 @@ export default {
   read(req, resource, params, config, callback) {
     try {
       const i18n = require(`../i18n/${params.locale}.js`);
+
       callback(null, i18n);
+      
+      if (process.env.NODE_ENV === 'development')
+        delete require.cache[require.resolve(`../i18n/${params.locale}.js`)];
+
     }
     catch(e) { callback(e); }
   }
