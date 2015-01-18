ba2e9e09dd824609973abf15240310637678206a
gpbl <io@gpbl.org>
2015-01-18 12:20:56 +0100

    Better notifications

--- webpack/plugins/notifyStats.js
+++ webpack/plugins/notifyStats.js
@@ -1,30 +1,10 @@
 // helpers for logging / notifying webpack stats 
-var chalk = require('chalk');
-var notifier = require('node-notifier');
-
 function notifyError(error) {
-  error = error.split('\n');
-  var title = error[error.length - 1];
-  var message = error[1];
-  console.log(chalk.red(title));
-  console.log('   ' + chalk.white(message));
-  notifier.notify({
-    title: title,
-    message: message,
-    sound: true
-  });
+  console.log('\x07', error);
 }
 
 function notifyWarning(warning) {
-  warning = warning.split('\n');
-  var title = warning[warning.length - 1];
-  var message = warning[1];
-  console.log(chalk.orange(title));
-  console.log('   ' + chalk.white(message));
-  notifier.notify({
-    title: title,
-    message: message
-  });
+  console.log(warning);
 }
 
 module.exports = function (stats) {
@@ -34,9 +14,9 @@ module.exports = function (stats) {
   else if (json.warnings.length > 0)
     json.warnings.forEach(notifyWarning);
   else
-  // just output the stats
+    // just output the stats
     console.log('\n' + stats.toString({
-    chunks: false,
-    colors: true
-  }));
+      chunks: false,
+      colors: true
+    }));
 };
