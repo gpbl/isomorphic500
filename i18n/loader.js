412e246e6e30b5f29cc63c009141ccb5b81cf149
gpbl <io@gpbl.org>
2015-01-22 00:52:05 +0100

    Make i18n loader load messageformat locales

--- i18n/loader.js
+++ i18n/loader.js
@@ -12,42 +12,27 @@ const loaders = {};
 
 // load italian
 loaders.it = function(done) {
-  
-  if (!hasIntl)
-    require.ensure([
-      'intl/locale-data/jsonp/it.js',
-      'moment/locale/it.js'
-    ], (require) => {
-      require('intl/locale-data/jsonp/it.js');
-      require('moment/locale/it.js');
-      done();
-    }, 'intl-it');
-  
-  else 
-    require.ensure([
-      'moment/locale/it.js'
-    ], (require) => {
-      require('moment/locale/it.js');
-      done();
-    }, 'it');
-  
+  require.ensure([
+    'intl/locale-data/jsonp/it.js',
+    'intl-messageformat/dist/locale-data/it.js';
+    'moment/locale/it.js'
+  ], (require) => {
+    require('intl/locale-data/jsonp/it.js');
+    require('intl-messageformat/dist/locale-data/it.js');
+    require('moment/locale/it.js');
+    done();
+  }, 'intl-it');
 }
 
 // load english
 loaders.en = function(done) {
-
-  if (!hasIntl)
-    require.ensure([
-      'intl/locale-data/jsonp/en.js'
-      // no need to load moment locale for en
-    ], (require) => {
-      require('intl/locale-data/jsonp/en.js');
-      done();
-    }, 'intl-en');
-  
-  else
+  require.ensure([
+    'intl/locale-data/jsonp/en.js'
+    // no need to load moment locale for en
+  ], (require) => {
+    require('intl/locale-data/jsonp/en.js');
     done();
-
+  }, 'intl-en');
 }
 
 // load i18n data for the given locale
