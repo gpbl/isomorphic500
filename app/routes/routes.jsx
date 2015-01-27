31800359a681f8fd432cc3b33d4b37033419e39e
Sebastian Ferrari <sebas5384@gmail.com>
2015-01-27 16:21:03 +0000

    remove simple page example, not ready yet

--- app/routes/routes.jsx
+++ app/routes/routes.jsx
@@ -8,15 +8,12 @@ var HomePage     = require('../pages/HomePage.jsx');
 var NotFoundPage = require('../pages/NotFoundPage.jsx');
 var UserPage     = require('../pages/UserPage.jsx');
 var RepoPage     = require('../pages/RepoPage.jsx');
-var SimplePage     = require('../pages/SimplePage.jsx');
 
 var routes = (
   <Router.Route name="app" path="/" handler={ App }>
     
-    <Router.Route name="simple-page" path="simple-page" handler={ SimplePage }/>
     <Router.Route name="user" path=":login" handler={ UserPage } />
     <Router.Route name="repo" path=":login/:name" handler={ RepoPage } />
-
 
     <Router.DefaultRoute name="home" handler={ HomePage } />
     <Router.NotFoundRoute name="notfound" handler={ NotFoundPage }/>
--- app/routes/server.jsx
+++ app/routes/server.jsx
@@ -10,11 +10,12 @@ var Html   = require('../pages/Html.jsx');
 module.exports = function (req, res, next) {
   Router.run(routes, req.path, function (Handler, state) {
     var title  = DocumentTitle.rewind();
-    // TODO: the next big thing...
-    // var markup = React.renderToString(<Handler />);
 
-    // var markup = '';
-    var html   = React.renderToStaticMarkup(<Html title={title}><Handler /></Html>);
+    var html   = React.renderToStaticMarkup(
+        <Html title={title}>
+            <Handler />
+        </Html>
+    );
 
     // TODO: send 404 status code 
     // (see: https://github.com/gpbl/isomorphic-react-template/issues/3)
