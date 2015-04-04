9fab6b463c2baefe7b428f3de7a7b695ab1647f1
gpbl <io@gpbl.org>
2015-04-04 14:43:27 +0200

    Better footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,8 +14,8 @@ class Footer extends React.Component {
           Data from <a href="https://500px.com">500px</a>. Photos copyrights of their respective authors.
         </div>
         <div>
-          <strong>isomorphic500</strong>. <a href="https://github.com/gpbl/isomorphic500">See the project’s page</a> or try something
-          exciting: <NavLink className="Footer-link" routeName="bad">a bad route</NavLink> or <NavLink className="Footer-link" routeName="photo" navParams={{id: 100000000000}}>an unexisting photo</NavLink>
+          <strong>isomorphic500</strong> is demo app built in <a href="https://facebook.github.io/react/">React</a> with <a href="http://www.fluxible.io">Fluxible</a>. See the <a href="https://github.com/gpbl/isomorphic500">project’s page</a> on Github or try something
+          exciting: <NavLink className="Footer-link" routeName="bad">a bad route</NavLink> or <NavLink className="Footer-link" routeName="photo" navParams={{id: 100000000000}}>an unexisting photo</NavLink>.
         </div>
       </div>
     );
