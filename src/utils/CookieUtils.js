9fdee0362b5a592d9baf0f678387c5277cdc5050
gpbl <io@gpbl.org>
2015-04-06 00:00:36 +0200

    Cookie utils

---
+++ src/utils/CookieUtils.js
@@ -0,0 +1,21 @@
+const CookieUtils = {
+
+  writeCookie(name, value, days) {
+    var expires;
+
+    if (days) {
+      var date = new Date();
+      date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+      expires = "; expires=" + date.toGMTString();
+    }
+    else {
+      expires = "";
+    }
+
+    document.cookie = name + "=" + value + expires + "; path=/";
+  }
+
+};
+
+export default CookieUtils;
+
