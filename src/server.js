8212be905c4f736faaa0c55fe68c07ec2c07933c
gpbl <io@gpbl.org>
2015-05-21 08:34:39 +0200

    Update for eslint-react

--- src/server.js
+++ src/server.js
@@ -71,7 +71,7 @@ if (server.get("env") === "development") {
 server.use(render);
 
 // Generic server errors (e.g. not caught by components)
-server.use((err, req, res, next) => {
+server.use((err, req, res, next) => {  // eslint-disable-line no-unused-vars
   console.log("Error on request %s %s", req.method, req.url);
   console.log(err);
   console.log(err.stack);
