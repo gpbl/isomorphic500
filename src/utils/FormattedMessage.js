6b19ac811739707fd31794422192e7f057db1fc6
Giampaolo Bellavite <io@gpbl.org>
2015-05-25 20:40:55 +0200

    Add some comments...

--- src/utils/FormattedMessage.js
+++ src/utils/FormattedMessage.js
@@ -1,3 +1,7 @@
+// Wrap react-intl's <FormattedMessage /> by passing messages and locales from the IntlStore 
+// Supports also the message props:
+// Example         <FormattedMessage message="home.welcome" />
+
 import { FormattedMessage } from "react-intl";
 import connectToIntlStore from "../utils/connectToIntlStore";
 
