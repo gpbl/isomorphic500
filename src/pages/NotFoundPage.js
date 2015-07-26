966284f461951ed02f38fca6c9514c57e5bbf481
gpbl <io@gpbl.org>
2015-07-26 18:53:12 +0200

    Fix FormatteMessage import (#71)

--- src/pages/NotFoundPage.js
+++ src/pages/NotFoundPage.js
@@ -1,5 +1,5 @@
 import React from "react";
-import FormattedMessage from "../utils/FormattedMessage";
+import { FormattedMessage } from "../utils/IntlComponents";
 
 class NotFoundPage extends React.Component {
 
