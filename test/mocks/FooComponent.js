5830caa5bed4de2900cd3849f36c4b55c9053b83
gpbl <io@gpbl.org>
2015-08-15 22:11:02 +0200

    Cleanup old stuff

--- test/mocks/FooComponent.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-class FooComponent extends React.Component {
-  render() {
-    return <div />;
-  }
-}
-
-export default FooComponent;
--- test/mocks/fooAction.js
+++
@@ -1,6 +0,0 @@
-function fooAction(context, payload, done) {
-  context.dispatch("FOO", payload);
-  done();
-}
-
-export default fooAction;
