5c8a4e37e636a3a6f886efbd8f1c262b5f6e960b
Giampaolo Bellavite <io@gpbl.org>
2015-10-08 22:28:37 +0200

    Code styling

--- src/constants/features.js
+++ src/constants/features.js
@@ -1,5 +1,12 @@
 // 500px featured photos
-const features = ["popular", "highest_rated", "upcoming",
-  "editors", "fresh_today", "fresh_yesterday", "fresh_week"];
+const features = [
+  "popular",
+  "highest_rated",
+  "upcoming",
+  "editors",
+  "fresh_today",
+  "fresh_yesterday",
+  "fresh_week"
+];
 
 export default features;
